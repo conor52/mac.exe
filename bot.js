53c72f7e918d7d6a89cbee7579a7a23611ad4c1c
conor52 <conormccarthy157@gmail.com>
2021-01-06 13:05:33 +0000

    first bot test

---
+++ bot.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in in ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.member.id === "276804557055721483") {
+    msg.react("👎")
+  }
+});
+
+
+
+client.login(process.env.BOT_TOKEN)
