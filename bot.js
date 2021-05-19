5edb64755f87f111296fa005225a64e14fa2ef0e
conor52 <conormccarthy157@gmail.com>
2021-05-19 09:28:13 +0100

    reverted changes to get bot online

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require("dotenv").config()
+
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -8,45 +9,44 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.member.id === '276804557055721483') {
-    msg.react('👎');
+  if (msg.member.id === "276804557055721483") {
+    msg.react("👎")
   }
 
-  if (msg.member.id === '140845637636718595') {
-    msg.react('👍');
+  if (msg.member.id === "140845637636718595") {
+    msg.react("👍")
   }
 
-  if (msg.member.id === '140833262065942528') {
-    msg.react('<:niall2:413768442005553152>');
+
+  if (msg.member.id === "140833262065942528") {
+    msg.react("<:niall2:413768442005553152>")
   }
 
-  if (msg.member.id === '378702852786356224') {
-    msg.react('🐀');
+  
+  if (msg.member.id === "378702852786356224") {
+    msg.react("🐀")
+  }
+  
+  
+  if (msg.member.id === "184793319014924289") {
+    msg.react("<:dion2:413724324671782925>")
   }
 
-  if (msg.member.id === '184793319014924289') {
-    msg.react('<:dion2:413724324671782925>');
-  }
-
-  if (msg.member.id === '140833262065942528') {
-    msg.react('<:niall2:413768442005553152>');
+  if (msg.member.id === "140833262065942528") {
+    msg.react("<:niall2:413768442005553152>")
   }
 
   if (msg.content === 'is edd a rat') {
-    msg.channel.send(
-      'I see absolutely no difference between edd and Stuart Little'
-    );
-  }
-
-  if (msg.content === 'test') {
-    message.channel.send('Hello Virgins', {
-      tts: true,
-    });
+    msg.channel.send('I see absolutely no difference between edd and Stuart Little');
   }
 
   if (msg.content === 'is bray a shithole') {
     msg.channel.send('yes, bray is shit.');
   }
+
+  
 });
 
-client.login(process.env.BOT_TOKEN);
+
+
+client.login(process.env.BOT_TOKEN)
