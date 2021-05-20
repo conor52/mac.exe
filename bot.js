a8715e6aff39320f8a9b398d5bbdd76011108e12
conor52 <conormccarthy157@gmail.com>
2021-05-20 11:18:25 +0100

    gweg

--- bot.js
+++ bot.js
@@ -5,62 +5,63 @@ const client = new discord.Client();
 const fs = require('fs');
 const path = require('path');
 
-client.on('message', async message => {
-  // Join the same voice channel of the author of the message
-  if (message.member.voice.channel) {
-    voice.channel.join().then(connection => {
-      connection.play(path.join(__dirname, 'hal.webm'));
+client.on('ready', () => {
+  console.log(`Logged in in ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'test') {
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      voice.channel.join().then(connection => {
+        connection.play(fs.createReadStream('hal.webm'), { type: 'webm/opus' });
+      });
+    }
+  }
+
+  if (msg.member.id === '276804557055721483') {
+    msg.react('👎');
+  }
+
+  if (msg.member.id === '140845637636718595') {
+    msg.react('👍');
+  }
+
+  if (msg.member.id === '140833262065942528') {
+    msg.react('<:niall2:413768442005553152>');
+  }
+
+  if (msg.member.id === '378702852786356224') {
+    msg.react('🐀');
+  }
+
+  if (msg.member.id === '184793319014924289') {
+    msg.react('<:dion2:413724324671782925>');
+  }
+
+  if (msg.member.id === '140833262065942528') {
+    msg.react('<:niall2:413768442005553152>');
+  }
+
+  if (msg.content === 'is edd a rat') {
+    msg.channel.send(
+      'I see absolutely no difference between edd and Stuart Little'
+    );
+  }
+
+  if (msg.content === 'is bray a shithole') {
+    msg.channel.send('yes, bray is shit.');
+  }
+
+  if (msg.content === 'test') {
+    const broadcast = client.voice.createBroadcast();
+    var channelId = msg.member.voice.channelID;
+    var channel = client.channels.cache.get(channelId);
+    channel.join().then(connection => {
+      broadcast.play(discordTTS.getVoiceStream('test 123'));
+      connection.play(fs.createReadStream('hal.webm'), { type: 'webm/opus' });
     });
   }
 });
-// client.on('ready', () => {
-//   console.log(`Logged in in ${client.user.tag}!`);
-// });
-
-// client.on('message', msg => {
-//   if (msg.member.id === '276804557055721483') {
-//     msg.react('👎');
-//   }
-
-//   if (msg.member.id === '140845637636718595') {
-//     msg.react('👍');
-//   }
-
-//   if (msg.member.id === '140833262065942528') {
-//     msg.react('<:niall2:413768442005553152>');
-//   }
-
-//   if (msg.member.id === '378702852786356224') {
-//     msg.react('🐀');
-//   }
-
-//   if (msg.member.id === '184793319014924289') {
-//     msg.react('<:dion2:413724324671782925>');
-//   }
-
-//   if (msg.member.id === '140833262065942528') {
-//     msg.react('<:niall2:413768442005553152>');
-//   }
-
-//   if (msg.content === 'is edd a rat') {
-//     msg.channel.send(
-//       'I see absolutely no difference between edd and Stuart Little'
-//     );
-//   }
-
-//   if (msg.content === 'is bray a shithole') {
-//     msg.channel.send('yes, bray is shit.');
-//   }
-
-//   if (msg.content === 'test') {
-//     const broadcast = client.voice.createBroadcast();
-//     var channelId = msg.member.voice.channelID;
-//     var channel = client.channels.cache.get(channelId);
-//     channel.join().then(connection => {
-//       broadcast.play(discordTTS.getVoiceStream('test 123'));
-//       connection.play(fs.createReadStream('hal.webm'), { type: 'webm/opus' });
-//     });
-//   }
-// });
 
 client.login(process.env.BOT_TOKEN);
