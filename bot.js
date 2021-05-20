a7383032ce75e805208c7f924258371357be6365
conor52 <conormccarthy157@gmail.com>
2021-05-20 14:53:16 +0100

    dion is stupid

--- bot.js
+++ bot.js
@@ -8,7 +8,22 @@ client.on('message', async message => {
   // Join the same voice channel of the author of the message
   if (message.member.voice.channel) {
     const connection = await message.member.voice.channel.join();
-    connection.play(fs.createReadStream('C:\Users\conor_mf0f\Dropbox\Vs code projects\mac.exe\Example.ogg'), { type: 'ogg/opus' });
+
+    // Create a dispatcher
+    const dispatcher = connection.play(fs.createReadStream('Example.ogg'), {
+      type: 'ogg/opus',
+    });
+
+    dispatcher.on('start', () => {
+      console.log('audio.mp3 is now playing!');
+    });
+
+    dispatcher.on('finish', () => {
+      console.log('audio.mp3 has finished playing!');
+    });
+
+    // Always remember to handle errors appropriately!
+    dispatcher.on('error', console.error);
   }
 });
 
